9c05ac0fe0c5ae66b57b30783923113d19d7ddf3
fzyexiaoxu <88084686@qq.com>
2018-05-10 09:40:02 +0800

    1

--- org.eclipse.mylyn.builds.ui/src/org/eclipse/mylyn/internal/builds/ui/view/BuildsView.java
+++ org.eclipse.mylyn.builds.ui/src/org/eclipse/mylyn/internal/builds/ui/view/BuildsView.java
@@ -321,25 +321,7 @@ public class BuildsView extends ViewPart implements IShowInTarget {
 	  		log.log("get notifyEventSource!");
 	  		if (notifyEventSource!=null) {
 	  		      log.log("notifyEventSource is not null:");
-	  		      notifyEventSource.addEventListener( new NotifyBuilderEventListener() {
-	  		      @Override
-        		  public void handleEvent(NotifyBuilderEvent de) {
-        		  	   String compontent = de.getCompontent();
-        		  	   log.log(compontent);
-                   
-        		  	   for (TreeItem parentItem : getViewer().getTree().getItems()) {
-        		  	   	 for (TreeItem item : parentItem.getItems()) {
-        		  	   		   if (item.getData() instanceof IBuildPlan) {
-        		  	   			   IBuildPlan plan = (IBuildPlan) item.getData();
-        		  	   			   if (plan.getSummary().equals(compontent)) {
-        		  	   				    IStructuredSelection selection = new StructuredSelection(plan);
-        		  	   				    getViewer().setSelection(selection);
-        		  	   			   }               
-        		  	   		   }
-        		  	   	 }                  			   
-        		     }
-        	     }
-        	   }  
+	  		      notifyEventSource.addEventListener( new NotifyBuilderEventListener()  
 	  		  );	  		      
 	      }
 	  }
