c8b53aed61ba25b813b4df662f91e90ff7cfdef5
fzyexiaoxu <88084686@qq.com>
2018-05-10 09:51:38 +0800

    1

---
+++ org.eclipse.mylyn.builds.ui/src/org/eclipse/mylyn/internal/event/NotifyBuilderEventListener.java
@@ -0,0 +1,14 @@
+package org.eclipse.mylyn.internal.event;
+ 
+import java.sql.*;
+import java.util.EventListener;
+
+public class  NotifyBuilderEventListener implements  NotifyBuilderEventListenerInterface {
+
+    @Override
+    public void handleEvent(NotifyBuilderEvent pe)
+    {
+
+    }
+
+}
